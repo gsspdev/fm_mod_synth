40a58015c7c9844bb61d18c300ad99f7eefbce1d
gsspdev <dev@gssp.io>
2023-12-29 02:16:43 -1000

    various changes

--- src/prompt_for_params.rs
+++ src/prompt_for_params.rs
@@ -16,11 +16,6 @@ impl OscillatorParams {
     }
 
 pub fn prompt_for_params() -> (OscillatorParams, OscillatorParams) {
-    // let osc1 = OscillatorParams {
-    //     amp: read_param("Enter amplitude for oscillator 1: "),
-    //     freq: read_param("Enter frequency for oscillator 1: "),
-    //     shape: read_param("Enter shape for oscillator 1 (sin, squ, saw, tri): "),
-    // };
     if std::env::args().len() == 4 {
         let amp = parse_arg::<f32>(&std::env::args().nth(1).unwrap(), "oscillator 1 amplitude"),
         let freq = parse_arg::<f32>(&std::env::args().nth(2).unwrap(), "oscillator 1 frequency"),
@@ -29,23 +24,6 @@ pub fn prompt_for_params() -> (OscillatorParams, OscillatorParams) {
         }
     } else {
         print!("ERROR: Invalid number of arguments. Please enter 3 arguments: amplitude, frequency, shape.")
-        // let osc_instance = OscillatorParams::new() {
-        //     let mut args = std::env::args().skip(1); // Skip the program name argument
-        // (
-        //     if let Some(value) = args.next() {
-        //         parse_arg::<f32>(&value, "oscillator 1 amplitude")
-        //     } else {
-        //         read_param::<f32>("Enter amplitude for oscillator 1: ")
-        //     },
-        //     if let Some(value) = args.next() {
-        //         parse_arg::<f32>(&value, "oscillator 1 frequency")
-        //     } else {
-        //         read_param::<f32>("Enter frequency for oscillator 1: ")
-        //     },
-        //     if let Some(value) = args.next() {
-        //         parse_arg::<String>(&value, "oscillator 1 shape")
-        //     } else {
-        //         read_param::<String>("Enter shape for oscillator 1 (sin, squ, saw, tri): ")
         }
     };
 
